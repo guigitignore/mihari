bb96b6a1671b74d8a6d0b20d8ee4c31f3c8c28a0
guigitignore <guillaume.chaye@yahoo.fr>
2024-06-03 23:48:50 +0200

    enrich with whois record

--- lib/mihari/enrichers/silentpush.rb
+++ lib/mihari/enrichers/silentpush.rb
@@ -1,32 +1,43 @@
 module Mihari
-    module Enrichers
+  module Enrichers
+    #
+    # SilentPush enricher
+    #
+    class SilentPush < Base
       #
-      # SilentPush enricher
+      # @param [Mihari::Models::Artifact] artifact
       #
-      class SilentPush < Base
-        #
-        # @param [Mihari::Models::Artifact] artifact
-        #
-        def call(artifact)
-          res = client.query(artifact.data)
-          Mihari.logger.info(res)
-          artifact.tap do |tapped|
-            
+      def call(artifact)
+        res = client.query(artifact.data)
+        Mihari.logger.info(res.inspect)
+
+        artifact.tap do |tapped|
+          domain_info = res.result&.domain_info
+          if domain_info
+
+            tapped.whois_record ||= Models::WhoisRecord.new(
+              domain: domain_info.domain,
+              registrar: {entity: domain_info.registrar},
+              created_on: Date.parse(domain_info.whois_created_date),
+              updated_on: Date.parse(domain_info.last_seen&.to_s)
+            )
           end
         end
-  
-        private
-        def callable_relationships?(artifact)
-          artifact.created_at.nil?
-        end
-  
-        def supported_data_types
-          %w[domain]
-        end
-  
-        def client
-          @client ||= Clients::SilentPush.new(key: Mihari.config.silentpush_api_key)
-        end
+      end
+
+      private
+
+      def callable_relationships?(artifact)
+        artifact.whois_record.nil?
+      end
+
+      def supported_data_types
+        %w[domain]
+      end
+
+      def client
+        @client ||= Clients::SilentPush.new(key: Mihari.config.silentpush_api_key)
       end
     end
   end
+end
--- lib/mihari/structs/silentpush.rb
+++ lib/mihari/structs/silentpush.rb
@@ -1,129 +1,128 @@
 # frozen_string_literal: true
 
 module Mihari
-    module Structs
-      module SilentPush
-        class DomainInfo < Dry::Struct
-          # @!attribute [r] age
-          #   @return [Integer,nil]
-          attribute :age, Types::Int.optional
+  module Structs
+    module SilentPush
+      class DomainInfo < Dry::Struct
+        # @!attribute [r] age
+        #   @return [Integer,nil]
+        attribute? :age, Types::Int.optional
 
-          # @!attribute [r] age_score
-          #   @return [Integer,nil]
-          attribute :age_score, Types::Int.optional
+        # @!attribute [r] age_score
+        #   @return [Integer,nil]
+        attribute? :age_score, Types::Int.optional
 
-          # @!attribute [r] domain
-          #   @return [String]
-          attribute :domain, Types::String
+        # @!attribute [r] domain
+        #   @return [String]
+        attribute :domain, Types::String
 
-          # @!attribute [r] first_seen
-          #   @return [Integer,nil]
-          attribute :first_seen, Types::Int.optional
+        # @!attribute [r] first_seen
+        #   @return [Integer,nil]
+        attribute? :first_seen, Types::Int.optional
 
-          # @!attribute [r] is_new
-          #   @return [Boolean,nil]
-          attribute :is_new, Types::Bool.optional
+        # @!attribute [r] is_new
+        #   @return [Boolean,nil]
+        attribute? :is_new, Types::Bool.optional
 
-          # @!attribute [r] is_new_score
-          #   @return [Integer,nil]
-          attribute :is_new_score, Types::Int.optional
+        # @!attribute [r] is_new_score
+        #   @return [Integer,nil]
+        attribute? :is_new_score, Types::Int.optional
 
-          # @!attribute [r] last_seen
-          #   @return [Integer,nil]
-          attribute :last_seen, Types::Int.optional
+        # @!attribute [r] last_seen
+        #   @return [Integer,nil]
+        attribute? :last_seen, Types::Int.optional
 
-          # @!attribute [r] query
-          #   @return [String]
-          attribute :query, Types::String
+        # @!attribute [r] query
+        #   @return [String]
+        attribute :query, Types::String
 
-          # @!attribute [r] registrar
-          #   @return [String]
-          attribute :registrar, Types::String
+        # @!attribute [r] registrar
+        #   @return [String]
+        attribute :registrar, Types::String
 
-          # @!attribute [r] whois_age
-          #   @return [Integer]
-          attribute :whois_age, Types::Int
+        # @!attribute [r] whois_age
+        #   @return [Integer]
+        attribute :whois_age, Types::Int
 
-          # @!attribute [r] whois_created_date
-          #   @return [String]
-          attribute :whois_created_date, Types::String
+        # @!attribute [r] whois_created_date
+        #   @return [String]
+        attribute :whois_created_date, Types::String
 
-          # @!attribute [r] zone
-          #   @return [String]
-          attribute :zone, Types::String
-  
-          class << self
-            #
-            # @param [Hash] d
-            #
-            def from_dynamic!(d)
-              d = Types::Hash[d]
-              Mihari.logger.info(d)
-              
-              new(
-                age: d.fetch("age"),
-                age_score: d.fetch("age_score"),
-                domain: d.fetch("domain"),
-                first_seen: d.fetch("first_seen"),
-                is_new: d.fetch("is_new"),
-                is_new_score: d.fetch("is_new_score"),
-                last_seen: d.fetch("last_seen"),
-                query: d.fetch("query"),
-                registrar: d.fetch("registrar"),
-                whois_age: d.fetch("whois_age"),
-                whois_created_date: d.fetch("whois_created_date"),
-                zone: d.fetch("zone")
-              )
-            end
+        # @!attribute [r] zone
+        #   @return [String]
+        attribute :zone, Types::String
+
+        class << self
+          #
+          # @param [Hash] d
+          #
+          def from_dynamic!(d)
+            d = Types::Hash[d]
+            Mihari.logger.info(d)
+
+            new(
+              age: d.fetch("age"),
+              age_score: d.fetch("age_score"),
+              domain: d.fetch("domain"),
+              first_seen: d.fetch("first_seen"),
+              is_new: d.fetch("is_new"),
+              is_new_score: d.fetch("is_new_score"),
+              last_seen: d.fetch("last_seen"),
+              query: d.fetch("query"),
+              registrar: d.fetch("registrar"),
+              whois_age: d.fetch("whois_age"),
+              whois_created_date: d.fetch("whois_created_date"),
+              zone: d.fetch("zone")
+            )
           end
         end
+      end
 
-        class Result < Dry::Struct
-          # @!attribute [r] domain_info
-          #   @return [DomainInfo]
-          attribute :domain_info, DomainInfo
-  
-          class << self
-            #
-            # @param [Hash] d
-            #
-            def from_dynamic!(d)
-              d = Types::Hash[d]
-              new(
-                domain_info: DomainInfo.from_dynamic!(d.fetch("domaininfo"))
-              )
-            end
+      class Result < Dry::Struct
+        # @!attribute [r] domain_info
+        #   @return [DomainInfo]
+        attribute :domain_info, DomainInfo
+
+        class << self
+          #
+          # @param [Hash] d
+          #
+          def from_dynamic!(d)
+            d = Types::Hash[d]
+            new(
+              domain_info: DomainInfo.from_dynamic!(d.fetch("domaininfo"))
+            )
           end
         end
-  
-        class Response < Dry::Struct
-          # @!attribute [r] code
-          #   @return [Integer]
-          attribute :status_code, Types::Int
-  
-          # @!attribute [r] status
-          #   @return [String,nil]
-          attribute :error, Types::String
-  
-          # @!attribute [r] result
-          #   @return [Result]
-          attribute :result, Result
-  
-          class << self
-            #
-            # @param [Hash] d
-            #
-            def from_dynamic!(d)
-              d = Types::Hash[d]
-              new(
-                code: d.fetch("status_code"),
-                error: d.fetch("error"),
-                result: Result.from_dynamic!(d.fetch("response"))
-              )
-            end
+      end
+
+      class Response < Dry::Struct
+        # @!attribute [r] code
+        #   @return [Integer]
+        attribute :status_code, Types::Int
+
+        # @!attribute [r] status
+        #   @return [String,nil]
+        attribute :error, Types::String.optional
+
+        # @!attribute [r] result
+        #   @return [Result]
+        attribute :result, Result
+
+        class << self
+          #
+          # @param [Hash] d
+          #
+          def from_dynamic!(d)
+            d = Types::Hash[d]
+            new(
+              status_code: d.fetch("status_code"),
+              error: d.fetch("error"),
+              result: Result.from_dynamic!(d.fetch("response"))
+            )
           end
         end
       end
     end
   end
-  
+end
